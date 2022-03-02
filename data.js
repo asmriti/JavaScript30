2931efbf8cdad30b4fe5d40b3067c09edce848a2
Smriti Aryal <smritiaryal331@gmail.com>
2022-03-03 00:25:45 +0545

    Flex Pannel Gallery

---
+++ data.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    title: "JavaScript Drum Kit",
+    link: "01 - JavaScript Drum Kit/",
+  },
+  {
+    title: "CSS + JS Clock",
+    link: "02 - JS and CSS Clock/",
+  },
+  {
+    title: "CSS Variables",
+    link: "03 - CSS Variables",
+  },
+  {
+    title: "Array Cardio",
+    link: "04 - Array Cardio Day 1",
+  },
+  {
+    title: "Flex Panel Gallery",
+    link: "05 - Flex Panel Gallery",
+  },
+];
